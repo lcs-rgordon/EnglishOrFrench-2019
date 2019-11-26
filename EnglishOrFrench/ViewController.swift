beeffbe2fe1620841709681a11fd57ea5a01fb7c
Russell Gordon <rgordon@lcs.on.ca>
2019-11-26 05:34:27 -0500

    Main storyboard, outlets, actions, all configured.

--- EnglishOrFrench/ViewController.swift
+++ EnglishOrFrench/ViewController.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties and Outlets
+    @IBOutlet weak var inputText: UITextView!
+    @IBOutlet weak var outputResults: UILabel!
 
+    // MARK: Initializers
+    
+    // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: Actions
+    @IBAction func analyze(_ sender: Any) {
+        
+        
+    }
+    
 
 
 }
